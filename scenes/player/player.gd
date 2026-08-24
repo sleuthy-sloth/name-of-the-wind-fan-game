b19b410cc6377a488347d7c6a116d31eb29f05d2
sleuthy-sloth <spkoehl@gmail.com>
2026-08-23 23:20:58 -0700

    swarm-lane: auto-commit before cleanup

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,11 +2,7 @@ extends CharacterBody2D
 
 @export var speed: float = 140.0
 
-func _ready() -> void:
-	print("DEBUG player _ready")
-
 func _physics_process(_delta: float) -> void:
 	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
-	print("DEBUG player velocity: " + str(velocity) + " pos: " + str(global_position))
 	move_and_slide()
